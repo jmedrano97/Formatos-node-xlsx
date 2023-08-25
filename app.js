e3d8bea1c4d44f98bf98bfa6fdfcf4876a37addb
Jaime Medrano <jaime08medrano@gmail.com>
2023-08-25 11:55:30 -0600

    ahora con modenas

--- app.js
+++ app.js
@@ -12,12 +12,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/generate-excel', (req, res) => {
-  // Crear un objeto de datos para el archivo Excel
+  // Función para convertir un número de serie de Excel a una fecha de JavaScript
+  const excelToDate = (excelDate) => {
+    const secondsInDay = 24 * 60 * 60 * 1000;
+    const excelEpoch = new Date(Date.UTC(1899, 11, 30));
+    return new Date(excelEpoch.getTime() + excelDate * secondsInDay);
+  };
+
+  // Modificar la matriz de datos para convertir los números de Excel en fechas
   const data = [
-    ['Nombre', 'Edad', 'Fecha de Nacimiento'],
-    ['Juan',   25, 42291  ],
-    ['María',  30, 42304  ],
-    ['Carlos', 22, 42307  ]
+    ['Nombre', 'Edad', 'Fecha de Nacimiento','dinero'],
+    ['Juan',   25, excelToDate(42291), 775000],
+    ['María',  30, excelToDate(42304), 450000],
+    ['Carlos', 22, excelToDate(42307), 1800000]
   ];
 
   // Crear un libro de Excel
